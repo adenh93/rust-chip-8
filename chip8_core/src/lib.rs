6f76dae4f2fd8eed7b459650e1b78b5f98c996f1
Aden Herold <aden.herold1@gmail.com>
2022-08-27 19:51:08 +1000

    added basic Emulator struct + required constants

--- chip8_core/src/lib.rs
+++ chip8_core/src/lib.rs
@@ -1,8 +1,76 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+pub const SCREEN_WIDTH: usize = 64;
+pub const SCREEN_HEIGHT: usize = 32;
+
+const START_ADDR: u16 = 0x200;
+const RAM_SIZE: usize = 4096;
+const REGISTER_COUNT: usize = 16;
+const STACK_SIZE: usize = 16;
+const NUM_KEYS: usize = 16;
+const FONTSET_SIZE: usize = 80;
+
+const FONTSET: [u8; FONTSET_SIZE] = [
+    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0 
+    0x20, 0x60, 0x20, 0x20, 0x70, // 1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+    0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+    0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+    0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+    0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+    0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+    0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+    0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+    0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+    0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+    0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+    0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+    0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+    0xF0, 0x80, 0xF0, 0x80, 0x80, // F
+];
+
+pub struct Emulator {
+    program_counter: u16,
+    ram: [u8; RAM_SIZE],
+    screen: [bool; SCREEN_WIDTH * SCREEN_HEIGHT],
+    v_register: [u8; REGISTER_COUNT],
+    i_register: u16,
+    stack_ptr: u16,
+    stack: [u16; STACK_SIZE],
+    keys: [bool; NUM_KEYS],
+    delay_timer: u8,
+    sound_timer: u8
+}
+
+impl Default for Emulator {
+    fn default() -> Self {
+        Self {
+            program_counter: START_ADDR,
+            ram: [0; RAM_SIZE],
+            screen: [false; SCREEN_WIDTH * SCREEN_HEIGHT],
+            v_register: [0; REGISTER_COUNT],
+            i_register: 0,
+            stack_ptr: 0,
+            stack: [0; STACK_SIZE],
+            keys: [false; NUM_KEYS],
+            delay_timer: 0,
+            sound_timer: 0
+        }
+    }
+}
+
+impl Emulator {
+    pub fn new() -> Self {
+        let mut emulator = Self::default();
+        emulator.ram[..FONTSET_SIZE].copy_from_slice(&FONTSET);
+        emulator
+    }
+
+    fn push(&mut self, val: u16) {
+        self.stack[self.stack_ptr as usize] = val;
+        self.stack_ptr += 1;
+    }
+
+    fn pop(&mut self) -> u16 {
+        self.stack_ptr -=1;
+        self.stack[self.stack_ptr as usize]
     }
 }
