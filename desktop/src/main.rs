cb2ba4ed3f0d4a8aa8de1ea40c9f1b5bc712dea8
Aden Herold <aden.herold1@gmail.com>
2022-09-01 20:19:59 +1000

    configured SDL2 canvas for frontend crate

--- desktop/src/main.rs
+++ desktop/src/main.rs
@@ -1,3 +1,38 @@
+use clap::Parser;
+use chip8_core::{SCREEN_WIDTH, SCREEN_HEIGHT, Emulator};
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about)]
+struct Args {
+    /// Path to ROM file
+    #[clap(value_parser)]
+    path: String,
+
+    /// Window scale amount
+    #[clap(short, long, value_parser, default_value_t = 15)]
+    scale: usize,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    let scaled_width = (SCREEN_WIDTH * args.scale) as u32;
+    let scaled_height = (SCREEN_HEIGHT * args.scale) as u32;
+
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let window = video_subsystem
+        .window("Chip-8 Emulator", scaled_width, scaled_height)
+        .position_centered()
+        .opengl()
+        .build()
+        .unwrap();
+
+    let mut canvas = window.into_canvas().present_vsync().build().unwrap();
+
+    canvas.clear();
+    canvas.present();
+
+    println!("Hello, {:?}!", args);
 }
